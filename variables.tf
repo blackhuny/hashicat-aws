eb6b39e5fac67db2f47e331fca4e210cdcca6c55
jhoh <blackhuny@daum.net>
2022-09-13 16:07:11 +0000

    Added s3 bucket module

--- variables.tf
+++ variables.tf
@@ -49,3 +49,9 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+    default     = ""
+}
