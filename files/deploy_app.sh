282a117508fd13407a2e7ab18d9805851ee60834
blackhuny <blackhuny@daum.net>
2022-09-13 15:28:48 +0900

    test
    main 문구 변경

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -12,6 +12,7 @@ cat << EOM > /var/www/html/index.html
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app.하시코프 너무어려워....
   <!-- END -->
 
   </div>
